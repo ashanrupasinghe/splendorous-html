1655a478da4e434b38fdf7db86fe32e754f99c73
ashan <ashanrupasinghe11@gmail.com>
2020-04-11 13:05:37 +0530

    slider v-1

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,26 @@
-/**/
+jQuery(Document).ready(function() {
+    "use strict"
+    $("#slider-carousle").carouFredSel({
+        responsive: true,
+        width: "100%" ,
+        circular: true,
+        scroll: {
+            items: 1,
+            duration: 500,
+            pouseOnHover: true
+        },
+        auto: true,
+        items: {
+            visible: {
+                min: 1,
+                max: 1
+            },
+            height: "variable"
+        },
+        pagination: {
+            container: ".sliderpager",
+            pageAnchorBuilder: false
+        }
+    }
+    );
+});
